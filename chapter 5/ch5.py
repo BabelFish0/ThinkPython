34eeb0acd9c49fd2858236cc7473d23d8049b74e
phajy <andy.young@bristol.ac.uk>
2022-10-22 12:43:25 -0400

    ex 5-1 done

--- chapter 5/ch5.py
+++ chapter 5/ch5.py
@@ -7,9 +7,9 @@ import time
 def convert(conversion, value):
     return value // conversion
 
-seconds = time.time() % 60
-minutes = convert(60, time.time()) % 60
-hours = convert(60*60, time.time()) % 24
-days = convert(60*60*24, time.time())
+seconds = str(time.time() % 60)
+minutes = str(convert(60, time.time()) % 60)
+hours = str(convert(60*60, time.time()) % 24)
+days = str(convert(60*60*24, time.time()))
 # %%
-print(days + " days, " + hours + " hours, " + days + " days " + " seconds since the epoch.")
+print(days + " days, " + hours + " hours, " + minutes + " minutes and " + seconds + " seconds since the epoch.")
