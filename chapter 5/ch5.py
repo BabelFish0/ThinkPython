b69ed74076b8d22a1607df0c90d88711896c5872
phajy <andy.young@bristol.ac.uk>
2022-10-22 12:51:04 -0400

    ex 5-2 done

--- chapter 5/ch5.py
+++ chapter 5/ch5.py
@@ -13,3 +13,16 @@ hours = str(convert(60*60, time.time()) % 24)
 days = str(convert(60*60*24, time.time()))
 # %%
 print(days + " days, " + hours + " hours, " + minutes + " minutes and " + seconds + " seconds since the epoch.")
+# %% [markdown]
+# ex 5-2
+# %%
+def check_fermat(a, b, c, n):
+    if n > 2 and a^n + b^n == c^n:
+        print("Holy smokes, Fermat was wrong!")
+    else:
+        print("No, that doesn't work.")
+# %%
+check_fermat(int(input('a\n')), int(input('b\n')), int(input('c\n')), int(input('n\n')))
+# %% [markdown]
+# ex 5-3
+# %%
