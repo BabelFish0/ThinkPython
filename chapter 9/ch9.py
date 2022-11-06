3674db17ba325565cd1be39841f17e30ed59d592
Jude Young <jude.d.l.young@icloud.com>
2022-11-06 11:17:28 +0000

    coded 9-2

--- chapter 9/ch9.py
+++ chapter 9/ch9.py
@@ -3,9 +3,9 @@
 # 9-1
 # %%
 fin = open("C:\\Users\\Jude Young\\Documents\\Coding\\DofE Project Gold\\ThinkPython\\chapter 9\\resources\\words.txt")
-for line in fin:
-    if len(line.strip()) > 20:
-        print(line.strip())
+# for line in fin:
+#     if len(line.strip()) > 20:
+#         print(line.strip())
 # %% [markdown]
 # 9-2
 # %%
@@ -14,3 +14,6 @@ def has_no_e(word):
         if letter == 'e':
             return False
     return True
+for line in fin:
+    if has_no_e(line.strip()):
+        print(line.strip())
