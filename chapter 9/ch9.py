80e085225124b35a7493662948f57fc7282b7e1e
Jude Young <jude.d.l.young@icloud.com>
2022-11-06 09:25:29 +0000

    9-1 done

--- chapter 9/ch9.py
+++ chapter 9/ch9.py
@@ -2,4 +2,7 @@
 # ch9
 # 9-1
 # %%
-fin = open("words.txt")
+fin = open("C:\\Users\\Jude Young\\Documents\\Coding\\DofE Project Gold\\ThinkPython\\chapter 9\\resources\\words.txt")
+for line in fin:
+    if len(line.strip()) > 20:
+        print(line.strip())
