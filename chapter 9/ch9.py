15574bc73d3296f98107ea899b52e1b9abc90424
Jude Young <jude.d.l.young@icloud.com>
2022-11-20 14:11:35 +0000

    finished mid-ch9 ex

--- chapter 9/ch9.py
+++ chapter 9/ch9.py
@@ -38,3 +38,43 @@ def num_avoids():
 #         rem_chars = chars[i+1:]
 
 #         for n in range(choose):
+# %% [markdown]
+# 9-4
+# %%
+def uses_only(str, word):
+    for letter in word:
+        if letter not in str:
+            return False
+    return True
+def words_using_only(str):
+    for line in fin:
+        if uses_only(str, line.strip()):
+            print(line.strip())
+#words_using_only('acefhlo')
+# %% [markdown]
+# 9-5
+# %%
+def uses_all(str, word):
+    for letter in str:
+        if letter not in word:
+            return False
+    return True
+def words_using_all(str):
+    for line in fin:
+        if uses_all(str, line.strip()):
+            print(line.strip())
+#words_using_all('aeiou')
+# %%
+# [markdown]
+# 9-6
+# %%
+def is_abecedarian(word):
+    for letter_index in range(0, len(word)-1): #start at 0, iterate to the penultimate letter so the last one is not checked against the first letter
+        if word[letter_index] > word[letter_index+1]:
+            return False
+    return True
+def all_isabc():
+    for line in fin:
+        if is_abecedarian(line.strip()):
+            print(line.strip())
+#all_isabc()
