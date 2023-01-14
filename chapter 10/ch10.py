e0f0ca1f7048c2297ce70e8221698c2f311f8db0
Jude Young <jude.d.l.young@icloud.com>
2023-01-14 17:46:41 +0000

    Modified ch10.py

--- chapter 10/ch10.py
+++ chapter 10/ch10.py
@@ -40,10 +40,50 @@ def excludeOuterValues(initialList:list) -> list:
     '''
     return initialList[1:-1]
 
+#10-4
+def chop(initialList:list) -> None:
+    '''
+    ### input:
+    list
+
+    ### modifies:
+    list excluding the 0th and -1th elements.
+    '''
+    del initialList[0]
+    del initialList[-1]
+
+#10-5
+def is_sorted(initialList:list) -> bool:
+    '''
+    ### input:
+    list
+
+    ### returns:
+    `True` if list is in ascending order and `False` if not.
+    '''
+    return initialList == sorted(initialList)
+
+#10-6
+def is_anagram(list1:list, list2:list) -> bool:
+    '''
+    ### input:
+    two lists
+
+    ### returns:
+    `True` if lists are anagrams of each other.
+    '''
+    return set(list1) == set(list2)
+
 def tests():
     print('Test input for 10-1: ', nestedSum([[1, 2], [3], [4, 5, 6]]))
     print('Test input for 10-2: ', cumulativeSum([1, 2, 3, 4]))
     print('Test input for 10-3: ', excludeOuterValues([1, 2, 3, 4]))
+    t = [1, 2, 3, 4]
+    print('Test input for 10-4: ', t)
+    chop(t)
+    print('Test output for 10-4: ', t)
+    print('Test input for 10-5: ', is_sorted([1, 2, 3]), is_sorted(['b', 'a']))
+    print('Test input for 10-6: ', is_anagram([1, 1, 3, 1], [1, 3, 1]))
 
 if __name__ == '__main__':
     tests()
